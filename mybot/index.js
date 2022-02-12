86d14a710ee2437dce51778282af8dc5071e1f6a
Кирилл <64669256+Kirill24052@users.noreply.github.com>
2022-02-12 14:39:00 +0300

    EDIT FILE
    EDIT FILE

--- mybot/index.js
+++ mybot/index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '1789464519:AAG1T8dejhOHPm7EjIN6_GWWKpaOAJAi9Qw';
+const token = 'TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
